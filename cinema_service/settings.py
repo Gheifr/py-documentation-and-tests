cb14744da4ec9858e1c25157fdfd81a9fb533656
Vitalii Prushak <48731758+Gheifr@users.noreply.github.com>
2026-02-26 18:50:00 +0200

    Update JWT auth

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -140,14 +140,17 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
 }
 SPECTACULAR_SETTINGS = {
     "TITLE": "Cinema Service API",
     "DESCRIPTION": "Order tickets for visiting movies",
     "VERSION": "1.0.0",
     "SERVE_INCLUDE_SCHEMA": False,
-    "SWAGGER_UI_SETTINGS":{
-      "deepLinking": True,
+    "SWAGGER_UI_SETTINGS": {
+        "deepLinking": True,
         "defaultModelRendering": "model",
         "defaultModelsExpandDepth": 2,
         "defaultModelExpandDepth": 2,
